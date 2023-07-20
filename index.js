e418050bf3772a9e314953de97119529bcc9ac35
YuichiOta <rb19780519@gmail.com>
2023-07-20 14:11:56 +0900

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 10000;
 require("dotenv").config();
 const cors = require("cors");
 
